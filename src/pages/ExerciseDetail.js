5278e9e6341d38590f70eac176b203b185d6be8a
sepehrzmn <95043491+sepehrzmn@users.noreply.github.com>
2022-06-26 16:54:47 +0430

    updated ExerciseDetail

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -15,6 +15,8 @@ const ExerciseDetail = () => {
 	const { id } = useParams();
 
 	useEffect(() => {
+		window.scrollTo({ top: "0", behavior: "smooth" });
+
 		const fetchExercisesData = async () => {
 			const exercisesDbUrl = "https://exercisedb.p.rapidapi.com";
 			const youtubeSearchUrl =
